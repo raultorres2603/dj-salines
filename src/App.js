9b427be55defeea047800c38355ff1f20de2e82c
Raul Torres <55553058+raultorres2603@users.noreply.github.com>
2022-11-14 15:21:44 +0100

    InitComit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import Cookies from "universal-cookie";
 
 function App() {
+  const [login, setLogin] = useState(false);
+  const [cookie, setCookie] = useState(new Cookies());
+
+  useEffect(() => {
+    if (cookie.get("Auth")) {
+      setLogin(true);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainMenu">
+      <div className="container">
+        <div className="col-md-12">{login ? <MainMenu /> : <Login />}</div>
+      </div>
     </div>
   );
 }
