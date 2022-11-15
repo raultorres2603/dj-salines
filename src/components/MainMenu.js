25c113f7efac798be7661c483cc5515bbc9ea90c
Raul Torres <55553058+raultorres2603@users.noreply.github.com>
2022-11-15 17:00:00 +0100

    .

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -92,7 +92,27 @@ export function MainMenu() {
                     <p className="text-center display-4">Elige</p>
                   </div>
                   <hr />
-                  <div className="listSongs"></div>
+                  <div className="listSongs">
+                    {songs.map((song, index) => (
+                      <div className="card">
+                        <img
+                          src={song.snippet.thumbnails.high.url}
+                          className="card-img-top"
+                          alt="..."
+                        />
+                        <div className="card-body">
+                          <h5 className="card-title">Card title</h5>
+                          <p className="card-text">
+                            Some quick example text to build on the card title
+                            and make up the bulk of the card's content.
+                          </p>
+                          <a href="#" className="btn btn-primary">
+                            Go somewhere
+                          </a>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
