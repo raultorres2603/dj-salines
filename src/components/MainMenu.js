c3f64d00f8098b072e1d25dd94103f4c533327c8
Raul Torres <55553058+raultorres2603@users.noreply.github.com>
2022-11-15 16:51:40 +0100

    Search Music

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,45 +1,100 @@
 import { useEffect, useState } from "react";
 import Cookies from "universal-cookie";
+import { YoutubeAPI } from "./YoutubeAPI";
 
 export function MainMenu() {
   const [username, setUsername] = useState("");
+  const [song, setSong] = useState("");
+  const [youtubeAPI, setAPI] = useState(new YoutubeAPI());
+  const [songs, setSongs] = useState([]);
 
   useEffect(() => {
     let cookie = new Cookies();
-    setUsername(cookie.get("Auth").user);
+    setUsername(cookie.get("User"));
     console.log(username);
   }, []);
 
+  async function searchSong() {
+    if (song.length > 0) {
+      let object = await youtubeAPI.getSongs(song);
+      setSongs(await object.items);
+      console.log(songs);
+    }
+  }
+
+  function handleInput(ev) {
+    switch (ev.target.id) {
+      case "titleSong":
+        setSong(ev.target.value);
+        break;
+
+      default:
+        break;
+    }
+  }
+
   return (
     <div className="card">
-      <div className="card-header display-3 text-center bg-info">
-        DJ-Salines
+      <div className="card-header bg-info">
+        <div className="row">
+          <p className="display-3 text-center fw-bold">DJ-Salines</p>
+        </div>
+        <div className="row">
+          <p className="h5 text-center fw-light">
+            Login: ({window.atob(username)})
+          </p>
+        </div>
       </div>
       <div className="card-body">
-        <div className="row">
-          <div className="col-sm-12">
-            <div className="row">
-              <div className="col-sm-6 border border-dark border-5 rounded">
-                <div className="row">
-                  <p className="text-center display-4">Busca tu canción</p>
-                </div>
-                <hr />
-                <div className="row">
-                  <div className="input-group mb-3">
-                    <span className="input-group-text" id="basic-addon1">
-                      🎼
-                    </span>
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="URL o título de la canción"
-                      aria-label="Username"
-                      aria-describedby="basic-addon1"
-                    />
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="row">
+                <div className="col-sm-6 border border-dark border-5 rounded">
+                  <div className="row">
+                    <p className="text-center display-4">Busca</p>
+                  </div>
+                  <hr />
+                  <div className="row">
+                    <div className="col-sm-6">
+                      <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon1">
+                          🎼
+                        </span>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="URL o título de la canción"
+                          aria-label="Username"
+                          aria-describedby="basic-addon1"
+                          onInput={handleInput}
+                          id="titleSong"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-sm-6">
+                      <div className="row">
+                        <div className="d-grid gap-2">
+                          <button
+                            type="button"
+                            className="btn btn-success"
+                            onClick={searchSong}
+                          >
+                            Confirmar
+                          </button>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
+                <div className="col-sm-6 border border-dark border-5 rounded">
+                  <div className="row">
+                    <p className="text-center display-4">Elige</p>
+                  </div>
+                  <hr />
+                  <div className="listSongs"></div>
+                </div>
               </div>
-              <div className="col-sm-6"></div>
             </div>
           </div>
         </div>
---
+++ src/components/YoutubeAPI.js
@@ -0,0 +1,15 @@
+export class YoutubeAPI {
+  constructor() {
+    this.apiKEY = "AIzaSyAeivme_yi0vF8jR0pkvwcFRBJXP1vhsII";
+    this.resultsPerPage = 5;
+  }
+
+  async getSongs(title) {
+    const url = `https://www.googleapis.com/youtube/v3/search?key=${this.apiKEY}&type=video&part=snippet&q=${title}&maxResults=${this.resultsPerPage}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+  }
+}
